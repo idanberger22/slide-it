e9a0842a2430c205897e301c327d3a99b961bc99
idan berger <idanberger22@gmail.com>
2022-07-13 12:01:50 +0300

    cleaning

--- src/pages/slide-app.jsx
+++ src/pages/slide-app.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { BOARD_SIZE, GRID_SIZE, TILE_COUNT } from "../constants"
-import { shuffle, canSwap, swap } from "../slide-service"
+import { shuffle, canSwap, swap, isSolved } from "../slide-service"
 import { Tile } from "../cmps/tile"
 
 export function SlideApp() {
     const [tiles, setTiles] = useState([...Array(TILE_COUNT).keys()])
-    const [isSolved, setIsSolved] = useState(false)
     const [isStarted, setIsStarted] = useState(false)
 
     const pieceHeight = Math.round(BOARD_SIZE / GRID_SIZE)
@@ -15,25 +14,22 @@ export function SlideApp() {
         height: BOARD_SIZE
     }
 
+    useEffect(() => {
+        if (isStarted && isSolved(tiles)){
+            setIsStarted(false)
+            console.log('you won!')
+        } 
+    }, [tiles])
+
     const shuffleTiles = () => {
         setTiles(shuffle(tiles))
-        console.log(tiles)
     }
 
     const swapTiles = (tileIndex) => {
         if (canSwap(tileIndex, tiles.indexOf(tiles.length - 1))) {
             const swapped = swap(tiles, tileIndex, tiles.indexOf(tiles.length - 1))
-            console.log(swapped)
             setTiles(swapped)
         }
-    }
-
-    const handleTileClick = (index) => {
-        swapTiles(index)
-    }
-
-    const handleShuffleClick = () => {
-        shuffleTiles()
     }
 
     const handleStart = () => {
@@ -43,19 +39,19 @@ export function SlideApp() {
 
     return (<>
         <ul style={style} className="board">
-        {tiles.map((tile, index) => (
-          <Tile
-            key={tile}
-            index={index}
-            tile={tile}
-            width={pieceWidth}
-            height={pieceHeight}
-            handleTileClick={handleTileClick}
-          />
-        ))}
-      </ul>
+            {tiles.map((tile, index) => (
+                <Tile
+                    key={tile}
+                    index={index}
+                    tile={tile}
+                    width={pieceWidth}
+                    height={pieceHeight}
+                    handleTileClick={swapTiles}
+                />
+            ))}
+        </ul>
         {!isStarted && <button className="center" onClick={handleStart}>start!</button>}
-        {isStarted && <button className="center" onClick={handleShuffleClick}>reset</button>}
+        {isStarted && <button className="center" onClick={shuffleTiles}>reset</button>}
     </>
     )
 }
